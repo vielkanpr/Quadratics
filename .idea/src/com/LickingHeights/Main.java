894ed2eb267f73eb3f7ee9cdba9dd442fbead236
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-26 10:00:07 -0400

    Initialized a,b,c

--- .idea/src/com/LickingHeights/Main.java
+++ .idea/src/com/LickingHeights/Main.java
@@ -8,7 +8,9 @@ public class Main {
         double a,b,c,discriminant,axisOfSymmetry,x,y;
         String parabolaFacing;
         //todo initialize manually
-
+        a=5;
+        b=3;
+        c=1;
         //todo calculations
 
         //todo print results
