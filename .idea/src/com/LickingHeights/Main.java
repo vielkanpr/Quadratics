c122a1e400e20eb2817d68e8367a00079fbe833a
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-26 09:53:23 -0400

    Added to do steps

--- .idea/src/com/LickingHeights/Main.java
+++ .idea/src/com/LickingHeights/Main.java
@@ -4,7 +4,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-      //write your code here
+        //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing,x,y
+
+        //todo initialize manually
+
+        //todo calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results
+
 
 
 
