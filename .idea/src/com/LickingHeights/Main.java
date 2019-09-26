a9e0490b41163a16459a4a1d75fdb88848bf02bd
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-26 10:24:06 -0400

    implemented if else ladder

--- .idea/src/com/LickingHeights/Main.java
+++ .idea/src/com/LickingHeights/Main.java
@@ -14,10 +14,18 @@ public class Main {
         //todo calculations
         axisOfSymmetry = -b/(2*a);
         discriminant = b*b-4*(a*c);
+        if(discriminant >0){
+            System.out.println("There are two real solutions");
+        }
+        else if (discriminant == 0){
+            System.out.println("There is one real solution");
+        }
+        else{
+            System.out.println("There are NO real solutions");
+        }
         //todo print results
         System.out.println("Axis of symmetry: "+ axisOfSymmetry);
         System.out.println("Discriminant: : "+ discriminant);
-        
         //todo not in code check results
 
         //todo initialize scanner and redo the initialize area
