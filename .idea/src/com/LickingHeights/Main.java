d145b87e9bb2f6ae04cf7c16d68aa805b3e96143
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-26 10:13:40 -0400

    Verified accuracy for axis and disriminant

--- .idea/src/com/LickingHeights/Main.java
+++ .idea/src/com/LickingHeights/Main.java
@@ -15,7 +15,9 @@ public class Main {
         axisOfSymmetry = -b/(2*a);
         discriminant = b*b-4*(a*c);
         //todo print results
-
+        System.out.println("Axis of symmetry: "+ axisOfSymmetry);
+        System.out.println("Discriminant: : "+ discriminant);
+        
         //todo not in code check results
 
         //todo initialize scanner and redo the initialize area
