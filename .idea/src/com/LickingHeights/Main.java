4068a8ae7d95699efc6895f506d0f425c01857bc
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-26 10:31:21 -0400

    Set up scanner

--- .idea/src/com/LickingHeights/Main.java
+++ .idea/src/com/LickingHeights/Main.java
@@ -1,5 +1,7 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -7,7 +9,15 @@ public class Main {
         //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing,x,y
         double a,b,c,discriminant,axisOfSymmetry,x,y;
         String parabolaFacing;
+        Scanner radar;
         //todo initialize manually
+        radar= new Scanner(System.in);
+        System.out.println("Give me an A");
+        a= radar.nextDouble( );
+        System.out.println("Give me an B");
+        b= radar.nextDouble( );
+        System.out.println("Give me an C");
+        c= radar.nextDouble( );
         a=5;
         b=3;
         c=1;
@@ -26,7 +36,6 @@ public class Main {
         //todo print results
         System.out.println("Axis of symmetry: "+ axisOfSymmetry);
         System.out.println("Discriminant: : "+ discriminant);
-        //todo not in code check results
 
         //todo initialize scanner and redo the initialize area
 
